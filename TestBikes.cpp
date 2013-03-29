3a79f257cf2c6ea694d9b8df8ccf334a4154b08b
andreyk <kostin@10205.ru>
2013-03-29 19:30:34 +0400

    += TestBikes

---
+++ TestBikes.cpp
@@ -0,0 +1,6 @@
+#include "TestBikes.h"
+
+namespace CppBikes
+{
+
+}
---
+++ TestBikes.h
@@ -0,0 +1,9 @@
+#ifndef _CPPBIKES_TESTBIKES_H_
+#define _CPPBIKES_TESTBIKES_H_
+
+namespace CppBikes
+{
+	//TODO: добавить тесты
+}
+
+#endif
