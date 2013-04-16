ff97db92815febc0eb2142baf3ef467d1463e0a1
andreyk <kostin@10205.ru>
2013-04-16 11:16:10 +0400

    += BikesStream (dev)

---
+++ BikesStream.cpp
@@ -0,0 +1,103 @@
+#include "BikesStream.h"
+#include "BasicGeometry.h"
+
+
+namespace CppBikes
+{
+	
+
+
+	void PointStreamer::read( ByteStream &bstr, Point* p )
+	{
+		read_(byteStream_(),p);
+	}
+
+	void PointStreamer::write( ByteStream &bstr, const Point* p )
+	{
+		write_(byteStream_(),p);
+	}
+
+	void PointStreamer::read( ByteStream &bstr, Point* p )
+	{
+		bstr >> p->gx;
+		bstr >> p->gy;
+		bstr >> p->gz;	
+	}
+
+	void PointStreamer::write( ByteStream &bstr, const Point* p )
+	{
+		bstr << p->gx;
+		bstr << p->gy;
+		bstr << p->gz;		
+	}
+
+
+	void VectorStreamer::read( ByteStream &bstr, Vector* v )
+	{
+		read(byteStream_(),v);
+	}
+
+	void VectorStreamer::read( ByteStream &bstr, Vector *v )
+	{
+		PointStreamer::read(bstr,v->anchor);
+		bstr >> v->gx;
+		bstr >> v->gy;
+		bstr >> v->gz;
+	}
+
+	void VectorStreamer::write( ByteStream &bstr, const Vector* v )
+	{
+		write(byteStream_(),v);
+	}
+
+	void VectorStreamer::write( ByteStream &bstr, const Vector *v )
+	{
+		PointStreamer::write(bstr,v->anchor);
+		bstr << v->gx;
+		bstr << v->gy;
+		bstr << v->gz;
+	}
+
+
+	void BasisStreamer::read( Basis* b )
+	{
+		read(byteStream_(),b);
+	}
+
+	void BasisStreamer::read( ByteStream &bstr, Basis *b )
+	{
+		PointStreamer::read(bstr,b->O);
+		b->i.anchor=b->O;
+		b->j.anchor=b->O;
+		b->k.anchor=b->O;
+		bstr >> b->i.gx;
+		bstr >> b->i.gy;
+		bstr >> b->i.gz;
+		bstr >> b->j.gx;
+		bstr >> b->j.gy;
+		bstr >> b->j.gz;
+		bstr >> b->k.gx;
+		bstr >> b->k.gy;
+		bstr >> b->k.gz;
+	}
+
+	void BasisStreamer::write( const Basis* b )
+	{
+		write(byteStream_(),b);
+	}
+
+	void BasisStreamer::write( ByteStream &bstr, const Basis *b )
+	{
+		PointStreamer::write(bstr,b->O);		
+		bstr << b->i.gx;
+		bstr << b->i.gy;
+		bstr << b->i.gz;
+		bstr << b->j.gx;
+		bstr << b->j.gy;
+		bstr << b->j.gz;
+		bstr << b->k.gx;
+		bstr << b->k.gy;
+		bstr << b->k.gz;
+	}
+
+} //CppBikes
---
+++ BikesStream.h
@@ -0,0 +1,57 @@
+#ifndef _CPPBIKES_BIKESSTREAM_H_
+#define _CPPBIKES_BIKESSTREAM_H_
+
+#include <CppBikes/ObjectSream.h>
+
+namespace CppBikes
+{
+	class Point;
+	class Vector;
+	class Basis;
+
+	class PointStreamer: public AbstractObjectStreamer<Point>
+	{
+	public:
+		virtual ~PointStreamer(){}
+		void read(Point* p);
+		void write(const Point* p);
+		
+		static void read(ByteStream &bstr, Point* p);
+		static void write(ByteStream &bstr, const Point* p);
+	};
+
+	class VectorStreamer: public AbstractObjectStreamer<Vector>
+	{
+	public:
+		virtual ~VectorStreamer(){}
+		void read(Vector* v);
+		void write(const Vector* v);
+
+		static void read(ByteStream &bstr, Vector *v);
+		static void write(ByteStream &bstr, const Vector *v);
+
+	};
+
+	class BasisStreamer: public AbstractObjectStreamer<Basis>
+	{
+	public:
+		virtual ~BasisStreamer(){}
+		void read(Basis* b);
+		void write(const Basis* b);
+
+		static void read(ByteStream &bstr, Basis *b);
+		static void write(ByteStream &bstr, const Basis *b);
+	};
+
+
+	class BikesStream: 
+		public AbstractObjectStream, 
+		public PointStreamer, 
+		public VectorStreamer,
+		public BasisStreamer
+	{
+	};
+
+} // CppBikes
+
+#endif
