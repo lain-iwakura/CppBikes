13b3db989b6c0bb4b0c805fa0366bc897cf55f80
andreyk <kostin@10205.ru>
2013-06-04 15:56:18 +0400

    SmartPtr += get()

--- include/Bikes/SmartPtr.h
+++ include/Bikes/SmartPtr.h
@@ -14,10 +14,47 @@ namespace Bikes
 		typedef typename boost::shared_ptr<T> Shared;
 		typedef typename boost::weak_ptr<T> Weak;
 		typedef typename std::auto_ptr<T> Auto;
+				
+	//--------------------------------------------------
+		template<class PtrT>
+		static T* get(const PtrT& ptr)
+		{
+			return ptr.get();
+		}
+
+		static T* get(T *p)
+		{
+			return p;
+		}
+
+		static const T* get(const T *p)
+		{
+			return p;
+		}
+
+		template<class PtrT>
+		static void reset(PtrT& ptr, T* p)
+		{
+			ptr.reset(p);
+		}
+
+		static void reset( T*& rp, T* p)
+		{
+			if(rp) delete rp;
+			rp=p;
+		}		
 	};
+
+	
+template<class TL, class TR>
+typename Ptr<TL>::Shared dynamic_ptr_cast(typename const Ptr<TR>::Shared & r)
+{
+	return boost::dynamic_pointer_cast<TL>(r);
+}
 
 }
 
-
+#define BIKES_GET_P(p) (&(*(p)))
+ 
 
 #endif
