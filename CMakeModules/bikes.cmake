fde9b7b8d128b65b5b1b9bd84e0c07845dcfde38
andreyk <kostin@10205.ru>
2013-12-02 20:17:52 +0400

    [CMake]  ввод в эксплуатацию CMakeBikes

---
+++ CMakeModules/bikes.cmake
@@ -0,0 +1,8 @@
+message(STATUS "\nInclude CMakeBikesModules.")
+include(bikes_messageFuncs)
+include(bikes_stringFuncs)
+include(bikes_fileFuncs)
+include(bikes_readAlien)
+include(bikes_aux)
+include(bikes_CMakeListsBody)
+include(bikes_useQt)
