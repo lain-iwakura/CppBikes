49f20555fd83616200ab3e34b268dfa126631b68
andreyk <andreykostin@inbox.ru>
2014-02-10 00:18:19 +0400

    [add] Geometry.h

---
+++ Bikes/include/Bikes/Geometry.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDEBIKES_GEOMETRY_H
+#define INCLUDEBIKES_GEOMETRY_H
+
+#include <Bikes/Geometry/Vector.h>
+#include <Bikes/Geometry/VectorPair.h>
+#include <Bikes/Geometry/VectorTriple.h>
+#include <Bikes/Geometry/Point.h>
+#include <Bikes/Geometry/Basis.h>
+
+#endif
+
