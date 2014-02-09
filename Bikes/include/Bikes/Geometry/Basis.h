c270ddca6663b9021c81e83c54fd2a3536bc2898
andreyk <andreykostin@inbox.ru>
2014-02-10 00:17:50 +0400

    [ref][up] Basis как тройка векторов ( вместо VectorTriple)

--- Bikes/include/Bikes/Geometry/Basis.h
+++ Bikes/include/Bikes/Geometry/Basis.h
@@ -1,22 +1,48 @@
-#ifndef INCLUDEBIKES_BASIS_H
-#define INCLUDEBIKES_BASIS_H
+#ifndef INCLUDEBIKES_VECTORTRIPLE_H
+#define INCLUDEBIKES_VECTORTRIPLE_H
 
 #include <Bikes/Geometry/Vector.h>
-#include <Bikes/Geometry/Point.h>
-#include <Bikes/Geometry/BasisReference.h>
+#include <Bikes/Geometry/VectorPair.h>
 
 namespace Bikes
 {
-class Basis: public BasisReference
+class VectorPair;
+
+class Basis
 {
 public:
 	Vector i;
 	Vector j;
 	Vector k;
-	Point O;
+
+	Basis(const Vector& vi, const Vector& vj, const Vector& vk);	
+
+	Basis(const VectorPair& pair_ij, const Vector& vk);
+
+	Basis(const Vector& vi, const VectorPair& pair_jk);
+
+	Basis(const Basis& ijk);
+
+	Basis(const Vector& vi, const Vector& vj, bool right=true);
+
+	Basis(const VectorPair& pair_ij, bool right=true);
+
+	virtual ~Basis();
+
+	void normalize();
+
+	void setScale(rnum scale);
+
+	void scaling(rnum scaleFactor);
+
+	void setOrtonormalByIJ(const Vector& vi, const Vector& vj, bool right=true);
+
+	void setOrtonormalByIJ(const VectorPair& ij, bool right=true);
 
 };
-}
 
+
+
+}
 
 #endif
--- Bikes/src/Geometry/Basis.cpp
+++ Bikes/src/Geometry/Basis.cpp
@@ -1,6 +1,87 @@
-#include <Bikes/Geometry/Basis.h>
+#include <Geometry/Basis.h>
+#include <Geometry/VectorPair.h>
 
 namespace Bikes
 {
 
-} // <- namespace Bikes
+Basis::Basis( const Vector& vi, const Vector& vj, const Vector& vk ):
+	i(vi),
+	j(vj),
+	k(vk)
+{
+}
+
+Basis::Basis( const VectorPair& pair_ij, const Vector& vk ):
+	i(pair_ij.i),
+	j(pair_ij.j),
+	k(vk)
+{
+}
+
+Basis::Basis( const Vector& vi, const VectorPair& pair_jk ):
+	i(vi),
+	j(pair_jk.i),
+	k(pair_jk.j)
+{
+}
+
+Basis::Basis( const Basis& ijk ):
+	i(ijk.i),
+	j(ijk.j),
+	k(ijk.j)
+{
+}
+
+Basis::Basis( const Vector& vi, const Vector& vj, bool right/*=true*/ )
+{
+	setOrtonormalByIJ(vi,vj,right);
+}
+
+Basis::Basis( const VectorPair& pair_ij, bool right/*=true*/ )
+{
+	setOrtonormalByIJ(pair_ij.i,pair_ij.j,right);
+}
+
+Basis::~Basis()
+{
+}
+
+void Basis::normalize()
+{
+	i.normalize();
+	j.normalize();
+	k.normalize();
+}
+
+void Basis::setScale( rnum scale )
+{
+	i.setLength(scale);
+	j.setLength(scale);
+	k.setLength(scale);
+}
+
+void Basis::scaling( rnum scaleFactor )
+{
+	i *= scaleFactor;
+	j *= scaleFactor;
+	k *= scaleFactor;
+}
+
+void Basis::setOrtonormalByIJ( const Vector& vi, const Vector& vj, bool right)
+{
+	i = vi;
+	k = vi*vj;
+	i.normalize();
+	k.normalize();
+	if(right)		
+		j = k*i;
+	else
+		j = i*k;
+}
+
+void Basis::setOrtonormalByIJ( const VectorPair& ij, bool right/*=true*/ )
+{
+	setOrtonormalByIJ(ij.i,ij.j,right);
+}
+
+}
