09dfee1e43744250992a2b022a3feb884d25bff4
andreyk <andreykostin@inbox.ru>
2014-02-21 02:11:11 +0400

    [up] HardTrAngle - реализован минимальный необходимый набор функций

--- Bikes/include/Bikes/Math/HardTrAngle.h
+++ Bikes/include/Bikes/Math/HardTrAngle.h
@@ -3,26 +3,85 @@
 
 #include <Bikes/Math/TrAngle.h>
 
+
 namespace Bikes
 {
 
-class HardTrAngle: TrAngle
+class HardTrAngle: public TrAngle
 {
 public:
-	HardTrAngle();
-	HardTrAngle(rnum _rad_val,rnum _sin_val, rnum _cos_val, rnum _tan_val);
-	HardTrAngle(rnum ang_rad);	
 
-	void set(rnum ang_rad);
-	HardTrAngle& operator =(rnum ang_rad);
 
-	operator rnum () const;
+    HardTrAngle();
+
+    HardTrAngle(radian ang);
+
+    HardTrAngle(cosnum cos_ang, ValSign sinSign);
+    
+              
+    sinnum sin() const;
+
+    cosnum cos() const;
+
+    //virtual tannum tan() const;
+
+    virtual radian rad() const;
+
+    //virtual degree deg() const;
+
+    virtual void set(const TrAngle& ang); //= 0;
+
+    void setRad(radian ang);
+
+    //virtual void setDeg(degree ang);
+
+    void setSin(sinnum sin_ang, ValSign cosSign = positiveSign);
+
+    void setCos(sinnum cos_ang, ValSign sinSign = positiveSign);
+
+    void setTan(tannum tan_ang, ValSign cosSign = positiveSign);
+
+    SoftTrAngle getSum(const TrAngle& ang) const;		
+
+    HardTrAngle& getSumAndSet(const TrAngle& ang);
+
+    SoftTrAngle getDifference(const TrAngle& ang) const ;
+
+    HardTrAngle& getDifferenceAndSet(const TrAngle& ang);
+
+    SoftTrAngle getProduct(rnum m) const;
+
+    SoftTrAngle getProduct(num n) const;
+
+    HardTrAngle& getProductAndSet(rnum m);
+
+    HardTrAngle& getProductAndSet(num n);
+
+    SoftTrAngle getNegative() const;
+
+    void setNegative();
+
+    //virtual bool isEqual(const TrAngle& ang) const; 
+
+    //virtual bool isLess(const TrAngle& ang) const;
+
+    //virtual bool isLessOrEqual(const TrAngle& ang) const;
+
+    //virtual bool isGreater(const TrAngle& ang) const;
+
+    //virtual bool isGreaterOrEqual(const TrAngle& ang) const;
+
+//     template< num n > 
+//     void getProductAndSetN()
+//     {
+//         ang.getProductAndSet(rnum(n));
+//     }
+
 
 private:
-	rnum _rad;
-	rnum _sin;
-	rnum _cos;
-	rnum _tan;
+    rnum _rad;
+    rnum _sin;
+    rnum _cos;
 };
 
 }
--- Bikes/src/Math/HardTrAngle.cpp
+++ Bikes/src/Math/HardTrAngle.cpp
@@ -1,8 +1,209 @@
 #include <Bikes/Math/HardTrAngle.h>
+#include <Bikes/BMath.h>
+
 
 namespace Bikes
 {
 
 
+
+
+HardTrAngle::HardTrAngle():
+    _sin(0.0),
+    _cos(1.0),
+    _rad(0.0)
+{
+}
+
+HardTrAngle::HardTrAngle( radian ang )
+{
+    setRad(ang);
+}
+
+HardTrAngle::HardTrAngle( cosnum cos_ang, ValSign sinSign )
+{
+    setCos(cos_ang,sinSign);
+}
+
+Bikes::sinnum HardTrAngle::sin() const
+{
+    return _sin;
+}
+
+Bikes::cosnum HardTrAngle::cos() const
+{
+    return _cos;
+}
+
+Bikes::radian HardTrAngle::rad() const
+{
+    return _rad;
+}
+
+void HardTrAngle::set( const TrAngle& ang )
+{
+    _sin = ang.sin();
+    _cos = ang.cos();
+    _rad = ang.rad();
+}
+
+void HardTrAngle::setRad( radian ang )
+{
+    _rad = ang;
+    _cos = ::cos(ang);
+    _sin = sqrt(1.0 - _cos*_cos);
+    if( normAngle(_rad) < 0 )
+        _sin = -_sin;
+}
+
+void HardTrAngle::setSin( sinnum sin_ang, ValSign cosSign /*= positiveSign*/ )
+{
+    _sin = inRange(sin_ang, -1.0, 1.0);
+    _cos = sqrt(1.0 - _sin*_sin);
+    
+    if( cosSign == negativeSign )
+        _cos = -_cos;
+
+    _rad = acos(_cos);
+
+    if( _sin < 0 )
+        _rad = - _rad;
+}
+
+void HardTrAngle::setCos( sinnum cos_ang, ValSign sinSign /*= positiveSign*/ )
+{
+    _cos = inRange(cos_ang, -1.0, 1.0);
+    _sin = sqrt(1.0 - _cos*_cos);
+    _rad = acos(_cos);
+
+    if(sinSign == negativeSign)
+    {
+        _sin = -_sin;
+        _rad = -_rad;
+    }    
+}
+
+void HardTrAngle::setTan( tannum tan_ang, ValSign cosSign /*= positiveSign*/ )
+{
+    rnum cc = 1.0/(1.0 + tan_ang*tan_ang);
+    _cos = sqrt(cc);
+    _sin = sqrt(1.0 - cc);
+    
+    if(cosSign == negativeSign)
+        _cos = -_cos;
+    
+    _rad = acos(_cos);
+    
+    if( (tan_ang < 0) == (_cos > 0) )
+    {
+        _sin = -_sin;
+        _rad = -_rad;
+    }
+}
+
+Bikes::SoftTrAngle HardTrAngle::getSum( const TrAngle& ang ) const
+{
+    rnum s = ang.sin();
+    rnum c = ang.cos();    
+
+    return SoftTrAngle(
+        _rad + ang.rad(),
+        _sin*c + _cos*_sin,
+        _cos*c - _sin*s
+        );
+}
+
+HardTrAngle& HardTrAngle::getSumAndSet( const TrAngle& ang )
+{
+    _rad += ang.rad();
+    
+    rnum s = ang.sin();
+    rnum c = ang.cos();
+    rnum save_sin=_sin;
+
+    _sin = _sin*c + _cos*s;
+    _cos = _cos*c - save_sin*s;
+
+    return *this;
+}
+
+Bikes::SoftTrAngle HardTrAngle::getDifference( const TrAngle& ang ) const
+{ 
+    rnum s = ang.sin();
+    rnum c = ang.cos();    
+
+    return SoftTrAngle(
+        _rad - ang.rad(),
+        _sin*c - _cos*s,
+        _cos*c + _sin*s
+        );
+}
+
+HardTrAngle& HardTrAngle::getDifferenceAndSet( const TrAngle& ang )
+{
+    _rad -= ang.rad();
+
+    rnum s = ang.sin();
+    rnum c = ang.cos();
+    rnum save_sin=_sin;
+
+    _sin = _sin*c - _cos*s;
+    _cos = _cos*c + save_sin*s;
+
+    return *this;
+}
+
+Bikes::SoftTrAngle HardTrAngle::getProduct( rnum m ) const
+{
+    return SoftTrAngle(m*_rad);
+}
+
+Bikes::SoftTrAngle HardTrAngle::getProduct( num n ) const
+{
+
+}
+
+HardTrAngle& HardTrAngle::getProductAndSet( rnum m )
+{    
+    setRad(m*_rad);
+    return *this;
+}
+
+
+typedef void (*nGetProductAndSet)(HardTrAngle& ang, num n);
+// 
+// template< num n>
+// void tGetProductAndSet(HardTrAngle& ang)
+// {
+//     ang.getProductAndSet(rnum(n));
+// }
+
+
+HardTrAngle& HardTrAngle::getProductAndSet( num n )
+{
+    if(n<0)
+    {
+
+    }else if(n == 0)
+    {
+    }else if(n == 1)
+    {
+    }else if(n == 2)
+    {
+
+    }
+    return *this;
+}
+
+Bikes::SoftTrAngle HardTrAngle::getNegative() const
+{
+    return SoftTrAngle(-_rad,-_sin,_cos);
+}
+
+void HardTrAngle::setNegative()
+{
+    _rad = -_rad;
+    _sin = -_sin;
+}
 
 }
