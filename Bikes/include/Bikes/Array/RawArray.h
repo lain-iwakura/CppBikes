b580f4f2a7fc02fad5ff81b512cc0b3c4545fac9
andreyk <andreykostin@inbox.ru>
2014-08-02 10:55:49 +0400

    [up] Array / RawArray - добавлен CreationSupervisor; максимальный инкремент выделения памяти убран из параметров шаблона

--- Bikes/include/Bikes/Array/RawArray.h
+++ Bikes/include/Bikes/Array/RawArray.h
@@ -2,110 +2,98 @@
 #define PREBIKES_RAWARRAY_H
 #include <Bikes/Types.h>
 #include <Bikes/Mathem/Tools.h>
+#include <Bikes/Creation/CreationSupervisor.h>
 #include <vector>
 
 namespace Bikes
 {
 
-template<class T, sznum maxCapacityIncrement_bytes = 1024>
+namespace Inner{
+
+const unum defMaxMemoryIncrement = 64*1024;
+
+template<typename T>
+const unum defMaxCapacityIncrement()
+{
+	static const unum inc = noLess<unum>(defMaxMemoryIncrement / sizeof(T), 1);
+	return inc;
+}
+
+}
+
+template<
+	class T,
+	class TCreationSupervisor = SimpleArraySupervisor<T>
+	>
 class RawArray
 {
 public:
 
-	RawArray():_arr(0),_cap(0),_sz(0)
+	typedef TCreationSupervisor CreationSupervisor;
+	
+	RawArray() :
+		_arr(0), 
+		_cap(0), 
+		_sz(0), 
+		_maxInc(Inner::defMaxCapacityIncrement<T>())
 	{
 	}
 
-	RawArray(sznum sz)
+	RawArray(sznum sz):
+		_arr(0),
+		_cap(sz),
+		_sz(sz),
+		_maxInc(Inner::defMaxCapacityIncrement<T>())
 	{
 		if(sz>0)
-		{	
-			_arr=new T[sz];
-			_cap=sz;
-			_sz=_cap;
-		}else
-		{
-			_arr=0;
-			_cap=0;
-			_sz=0;
-		}
+			_arr = _createArray(sz);
 	}
 
-	RawArray(sznum sz, sznum cap)
+	RawArray(sznum sz, sznum cap):
+		_arr(_createArray(cap)),
+		_cap(cap),
+		_sz(noGreater<sznum>(sz,cap)),
+		_maxInc(Inner::defMaxCapacityIncrement<T>())
 	{
-		if(cap>0)
-		{
-			_cap=cap;
-			_arr=new T[cap];
-			if(sz<=cap)
-			{
-				if(sz>0) _sz=sz;
-				else _sz=0;
-			}else _sz=cap;
-		}else
-		{
-			_arr=0;
-			_cap=0;
-			_sz=0;
-		}		
 	}
 
-	RawArray(const RawArray<T>& ra): _cap(ra._cap), _sz(ra._sz)
+	RawArray(sznum sz, sznum cap, sznum maxCapInc) :
+		_arr(_createArray(cap)),
+		_cap(cap),
+		_sz(noGreater<sznum>(sz, cap)),
+		_maxInc(maxCapInc)
 	{
-		if(_cap>0)
-		{		
-			_arr=new T[_cap];
-			for(sznum i=0; i<_sz; i++)
-			{
-				_arr[i]=ra._arr[i];
-			}
-		}else
-		{
-			_arr=0;
-		}
 	}
 
-	RawArray(const T* buf, sznum sz)
+	RawArray(const RawArray& ra):
+		_arr(_createArrayCopy(ra._arr,ra._cap,ra._sz)),
+		_cap(ra._cap), 
+		_sz(ra._sz),
+		_maxInc(ra._maxInc)
 	{
-		if(sz>0)
-		{
-			_arr=new T[sz];
-			for(sznum i=0; i<sz; i++)
-				_arr[i]=buf[i];	
-			_sz=sz;
-			_cap=sz;
-		}else
-		{
-			_arr=0;
-			_sz=0;
-			_cap=0;
-		}
+	}
+
+	RawArray(const T* arr, sznum sz):
+		_arr(_createArrayCopy(arr,sz,sz)),
+		_cap(sz),
+		_sz(sz),
+		_maxInc(Inner::defMaxCapacityIncrement<T>())
+	{
 	}
 
 	virtual ~RawArray()
 	{
-		if(_arr) 
-			delete [] _arr;
+		_destroy(_arr);
 	}
 
 	void setCapacity(sznum cap)
 	{
-		T *narr=0;
-		if(cap>0)
-		{		
-			narr=new T[cap];
-			sznum ms=_sz;if(ms>cap)ms=cap;
-			for(sznum i=0; i<ms;i++)
-				narr[i]=_arr[i];
-			_cap=cap;
-			_sz=ms;
-		}else
-		{
-			_cap=0;
-			_sz=0;
-		}
-		if(_arr) delete [] _arr;
-		_arr=narr;		
+		_cap = cap;
+		if (_sz > cap)
+			_sz = cap;
+		T *narr=_createArrayCopy(_arr,cap,_sz);
+		_destroy(_arr);
+		_arr = narr;
 	}
 
 	void setSize(sznum sz)
@@ -118,21 +106,22 @@ public:
 	void push_back(const T& val)
 	{
 		if (_cap == _sz)
-		{
-			setCapacity(_cap + inRange(_cap, 1, maxCapacityIncrement()));
-		}
-		setSize(_sz+1);
+			setCapacity(_cap + inRange<sznum>(_cap, 1, _maxInc));
+		_sz+=1;
 		_arr[_sz-1]=val;
 	}
 
 	void pop_back()
 	{
-		setSize(_cap-1);
+		if (sz!=0)
+			_sz--;
 	}
 	
 	void clear()
 	{
-		setSize(0);
+		_sz=0;
+		if (_cap > _maxInc)
+			setCapacity(_maxInc);
 	}
 
 	sznum size() const
@@ -155,25 +144,23 @@ public:
 		return _arr;
 	}
 
-	void takeData(T* d, sznum sz)
+	void take(T* d, sznum sz)
 	{
-		 if(_arr) 
-			 delete [] _arr;
+		_destroy(_arr);
 		 _arr=d;
 		 _cap=sz;
 		 _sz=_cap;
 	}
 
-	void takeData(RawArray<T>& ra)
+	void take(RawArray& ra)
 	{
-		if(_arr) 
-			delete [] _arr;
+		_destroy(_arr);
 		_arr=ra.arr;
-		_cap=ra.s;
-		_sz=ra.l;
-		ra.arr=0;
-		ra.s=0;
-		ra.l=0;
+		_cap=ra._cap;
+		_sz=ra._sz;
+		ra.arr=0; 
+		ra._sz=0;
+		ra._cap=0;
 	}
 
 	T* passData() // unsafe!
@@ -216,22 +203,87 @@ public:
 	}
 
 	
-	static sznum maxCapacityIncrement()
+	unum maxCapacityIncrement() const
 	{
-		static const sznum inc = 
-			inRange<sznum>(
-			maxCapacityIncrement_bytes / sizeof(T), 
-			1, 
-			maxCapacityIncrement_bytes
-			);
+		return _maxInc;
+	}
 
-		return inc;
+	unum maxMemoryIncrement() const
+	{
+		return _maxInc*sizeof(T);
+	}
+
+	void setMaxCapacityIncrement(unum inc_size)
+	{
+		_maxInc = noLess<unum>(inc_size, 1);
+	}
+
+	void setMaxMemoryIncrement(unum inc_bytes)
+	{
+		_maxInc = setMaxCapacityIncrement(inc_bytes / sizeof(T));
+	}
+
+	bool operator == (const RawArray& other) const
+	{
+		if (other._sz != _sz)
+			return false;
+		for (sznum i = 0; i < _sz; i++)
+		{
+			const T &v1 = _arr[i];
+			const T &v2 = other[i];
+			if (other[i] != _arr[i])
+				return false;
+		}
+		return true;
+	}
+
+	bool operator !=(const RawArray& other) const
+	{
+		return !(*this == other);
 	}
 
 private:
+
+	static T* _createCopy(const T* obj)
+	{
+		return CreationSupervisor::createCopy(obj);
+	}
+
+	static T* _create()
+	{
+		return CreationSupervisor::create();
+	}
+
+	static T* _createArray(sznum sz)
+	{
+		if (sz == 0)
+			return 0;
+		return CreationSupervisor::createArray(sz);
+	}
+
+	static T* _createArrayCopy(const T* arr, sznum cap, sznum sz)
+	{
+		if (cap == 0)
+			return 0;
+		T* carr = CreationSupervisor::createArray(cap);
+		if (arr)
+		{
+			for (sznum i = 0; i < sz; i++)
+				carr[i] = arr[i];
+		}
+		return carr;
+	}
+
+	static void _destroy(T* arr)
+	{
+		if (arr)
+			CreationSupervisor::destroy(arr);
+	}
+
 	T *_arr;
 	sznum _cap;	
 	sznum _sz;
+	unum _maxInc;
 };
 
 
