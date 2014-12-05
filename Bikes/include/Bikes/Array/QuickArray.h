910e7c191cce4e7454f04a88629fbb7f821273eb
andreyk <andreykostin@inbox.ru>
2014-12-06 00:38:26 +0300

    [up] Array / QuickArray

--- Bikes/include/Bikes/Array/QuickArray.h
+++ Bikes/include/Bikes/Array/QuickArray.h
@@ -1,5 +1,5 @@
-#ifndef PREBIKES_QUICKARRAY_H
-#define PREBIKES_QUICKARRAY_H
+#ifndef INCLUDE_BIKES_ARRAY_QUICKARRAY_H
+#define INCLUDE_BIKES_ARRAY_QUICKARRAY_H
 #include <Bikes/MacrosBikes.h>
 
 #define AUXBIKES_QUICKARRAY_FILL_1(ARR,EL) ARR[0]=EL##1;
@@ -11,17 +11,44 @@
 #define AUXBIKES_QUICKARRAY_FILL_7(ARR,EL) ARR[0]=EL##1; ARR[1]=EL##2; ARR[2]=EL##3; ARR[3]=EL##4; ARR[4]=EL##5; ARR[5]=EL##6; ARR[6]=EL##7;
 #define AUXBIKES_QUICKARRAY_FILL_8(ARR,EL) ARR[0]=EL##1; ARR[1]=EL##2; ARR[2]=EL##3; ARR[3]=EL##4; ARR[4]=EL##5; ARR[5]=EL##6; ARR[6]=EL##7; ARR[7]=EL##8;
 #define AUXBIKES_QUICKARRAY_FILL_9(ARR,EL) ARR[0]=EL##1; ARR[1]=EL##2; ARR[2]=EL##3; ARR[3]=EL##4; ARR[4]=EL##5; ARR[5]=EL##6; ARR[6]=EL##7; ARR[7]=EL##8; ARR[8]=EL##9;
-//#define AUXBIKES_QUICKARRAY_FILL_10(ARR,EL) ARR[0]=EL##1; ARR[1]=EL##2; ARR[2]=EL##3; ARR[3]=EL##4; ARR[4]=EL##5; ARR[5]=EL##6; ARR[6]=EL##7; ARR[7]=EL##8; ARR[8]=EL##9; ARR[9]=EL##10;
 
 namespace Bikes
 {
-	template<class T, int n>
-	struct QuickArray
+
+	template<class T, sznum n>
+	class QuickArray
 	{
-		operator T*(){return arr;}
-		operator const T*() const {return arr;}
-		T& operator[](int i){return arr[i];}
-		const T& operator[](int i) const{return arr[i];}
+	public:
+
+		QuickArray()
+		{
+		}
+
+		operator T*()
+		{
+			return arr;
+		}
+
+		operator const T*() const 
+		{
+			return arr;
+		}
+
+		T& operator[](int i)
+		{
+			return arr[i];
+		}
+
+		const T& operator[](int i) const
+		{
+			return arr[i];
+		}
+
+		sznum size() const 
+		{
+			return n;
+		}
+
 		T arr[n];
 	};
 
@@ -36,15 +63,7 @@ namespace Bikes
 		return arr;\
 	}
 
-AUXBIKES_QUICK_ARRAY(1)
-AUXBIKES_QUICK_ARRAY(2)
-AUXBIKES_QUICK_ARRAY(3)
-AUXBIKES_QUICK_ARRAY(4)
-AUXBIKES_QUICK_ARRAY(5)
-AUXBIKES_QUICK_ARRAY(6)
-AUXBIKES_QUICK_ARRAY(7)
-AUXBIKES_QUICK_ARRAY(8)
-AUXBIKES_QUICK_ARRAY(9)
+MACROSBIKES_FOREACHPARAMLIST(AUXBIKES_QUICK_ARRAY)
 
 //===================================================================
 
@@ -57,16 +76,7 @@ AUXBIKES_QUICK_ARRAY(9)
 	return arr;\
 }
 
-
-AUXBIKES_QUICK_ARRAY_VAL(1)
-AUXBIKES_QUICK_ARRAY_VAL(2)
-AUXBIKES_QUICK_ARRAY_VAL(3)
-AUXBIKES_QUICK_ARRAY_VAL(4)
-AUXBIKES_QUICK_ARRAY_VAL(5)
-AUXBIKES_QUICK_ARRAY_VAL(6)
-AUXBIKES_QUICK_ARRAY_VAL(7)
-AUXBIKES_QUICK_ARRAY_VAL(8)
-AUXBIKES_QUICK_ARRAY_VAL(9)
+MACROSBIKES_FOREACHPARAMLIST(AUXBIKES_QUICK_ARRAY_VAL)
 
 //===================================================================
 
@@ -79,15 +89,7 @@ AUXBIKES_QUICK_ARRAY_VAL(9)
 	return arr;\
 	}
 
-AUXBIKES_QUICK_VECTOR(1)
-AUXBIKES_QUICK_VECTOR(2)
-AUXBIKES_QUICK_VECTOR(3)
-AUXBIKES_QUICK_VECTOR(4)
-AUXBIKES_QUICK_VECTOR(5)
-AUXBIKES_QUICK_VECTOR(6)
-AUXBIKES_QUICK_VECTOR(7)
-AUXBIKES_QUICK_VECTOR(8)
-AUXBIKES_QUICK_VECTOR(9)
+MACROSBIKES_FOREACHPARAMLIST(AUXBIKES_QUICK_VECTOR)
 
 //===================================================================
 
@@ -100,16 +102,7 @@ AUXBIKES_QUICK_VECTOR(9)
 		return arr;\
 	}
 
-AUXBIKES_QUICK_VECTOR_VAL(1)
-AUXBIKES_QUICK_VECTOR_VAL(2)
-AUXBIKES_QUICK_VECTOR_VAL(3)
-AUXBIKES_QUICK_VECTOR_VAL(4)
-AUXBIKES_QUICK_VECTOR_VAL(5)
-AUXBIKES_QUICK_VECTOR_VAL(6)
-AUXBIKES_QUICK_VECTOR_VAL(7)
-AUXBIKES_QUICK_VECTOR_VAL(8)
-AUXBIKES_QUICK_VECTOR_VAL(9)
+MACROSBIKES_FOREACHPARAMLIST(AUXBIKES_QUICK_VECTOR_VAL)
 
-
-} // CppBikes
-#endif
+} 
+#endif // <- INCLUDE_BIKES_ARRAY_QUICKARRAY_H
