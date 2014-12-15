7e88106faedd348641c68a0e3c1a01512e24c737
andreyk <andreykostin@inbox.ru>
2014-12-15 18:01:47 +0300

    [up] TypeStack

--- Bikes/include/Bikes/TypeCollecting/TypeStack.h
+++ Bikes/include/Bikes/TypeCollecting/TypeStack.h
@@ -48,7 +48,7 @@ template<class T1, class T2>
 struct NotNull<Element<T1,T2> >
 {
     typedef typename SelectType<
-        TypeEqual<typename Element<T1, T2>::HeadType, NullType>::result,
+        TypeEqual<typename Element<T1, T2>::Head, NullType>::result,
         NullType,
         Element<T1, T2>
         >
@@ -58,49 +58,49 @@ struct NotNull<Element<T1,T2> >
 template<class LeftT, class  RightT>
 struct Element
 {
-    typedef LeftT HeadType;
-    typedef Element<RightT,NullType> TailType;
+    typedef LeftT Head;
+    typedef Element<RightT,NullType> Tail;
 
 //     typedef typename SelectType<
 //         Detector<RightT>::isTypeStack || TypeEqual<RightT, NullType>::result,
 //         RightT,
 //         Element<RightT, NullType>
 //     >
-//     ::ResultType TailType;
+//     ::ResultType Tail;
 };
 //------------------------------------------------------------------------------
 template<class T>
 struct Element<NullType, T>
 {
-    typedef T HeadType;
-    typedef NullType TailType;
+    typedef T Head;
+    typedef NullType Tail;
 };
 //------------------------------------------------------------------------------
 template<class T>
 struct Element<T, NullType>
 {
-    typedef T HeadType;
-    typedef NullType TailType;
+    typedef T Head;
+    typedef NullType Tail;
 };
 //------------------------------------------------------------------------------
 template<>
 struct Element<NullType, NullType>
 {
-    typedef NullType HeadType;
-    typedef NullType TailType;
+    typedef NullType Head;
+    typedef NullType Tail;
 };
 //------------------------------------------------------------------------------
 template<class T1, class T2, class T3 >
 struct Element<T1,Element<T2, T3> >
 {
-    typedef T1 HeadType;
-    typedef typename NotNull<Element<T2, T3> >::ResultType TailType;
+    typedef T1 Head;
+    typedef typename NotNull<Element<T2, T3> >::ResultType Tail;
 };
 template<class T1, class T2 >
 struct Element<NullType, Element<T1, T2> >
 {
-    typedef typename Element<T1, T2>::HeadType HeadType;
-    typedef typename Element<T1, T2>::TailType TailType;
+    typedef typename Element<T1, T2>::Head Head;
+    typedef typename Element<T1, T2>::Tail Tail;
 };
 //------------------------------------------------------------------------------
 template<class T1, class T2, class T3 >
@@ -108,42 +108,42 @@ struct Element<Element<T1, T2>, T3>
 {
 
     typedef typename SelectType<
-    TypeEqual<typename Element<T1, T2>::HeadType, NullType>::result,
+    TypeEqual<typename Element<T1, T2>::Head, NullType>::result,
     T3,
-    typename Element<T1, T2>::HeadType
+    typename Element<T1, T2>::Head
     >
-    ::ResultType HeadType;
+    ::ResultType Head;
 
-    typedef Element<typename Element<T1, T2>::TailType, T3> TailType;
+    typedef Element<typename Element<T1, T2>::Tail, T3> Tail;
 };
 //------------------------------------------------------------------------------
 template<class T1, class T2>
 struct Element<Element<T1, T2>, NullType>
 {
-    typedef typename Element<T1, T2>::HeadType HeadType;
-    typedef typename Element<T1, T2>::TailType TailType;
+    typedef typename Element<T1, T2>::Head Head;
+    typedef typename Element<T1, T2>::Tail Tail;
 };
 //------------------------------------------------------------------------------
 template<class T1, class T2, class T3, class T4>
 struct Element<Element<T1, T2>, Element<T3,T4> >
 {
     typedef typename SelectType<
-    TypeEqual<typename Element<T1, T2>::HeadType, NullType>::result,
-    typename Element<T3, T4>::HeadType,
-    typename Element<T1, T2>::HeadType
+    TypeEqual<typename Element<T1, T2>::Head, NullType>::result,
+    typename Element<T3, T4>::Head,
+    typename Element<T1, T2>::Head
     >
-    ::ResultType HeadType;
+    ::ResultType Head;
 
     typedef typename SelectType<
-        TypeEqual<typename Element<T1, T2>::HeadType, NullType>::result,
-        typename Element<T3, T4>::TailType,
-        Element<typename Element<T1, T2>::TailType, Element<T3, T4> >
+        TypeEqual<typename Element<T1, T2>::Head, NullType>::result,
+        typename Element<T3, T4>::Tail,
+        Element<typename Element<T1, T2>::Tail, Element<T3, T4> >
         >
-        ::ResultType TailType;
+        ::ResultType Tail;
 };
 //==============================================================================
 template<class T>
-struct Lenght
+struct Length
 {
     enum{
         result = 1
@@ -151,7 +151,7 @@ struct Lenght
 };
 //------------------------------------------------------------------------------
 template<>
-struct Lenght<NullType>
+struct Length<NullType>
 {
     enum{
         result = 0
@@ -159,16 +159,16 @@ struct Lenght<NullType>
 };
 //------------------------------------------------------------------------------
 template<class LeftT, class RightT>
-struct Lenght<Element<LeftT, RightT> >
+struct Length<Element<LeftT, RightT> >
 {
     enum{
-        result = Lenght<typename Element<LeftT, RightT>::HeadType>::result
-        + Lenght<typename Element<LeftT, RightT>::TailType >::result
+        result = Length<typename Element<LeftT, RightT>::Head>::result
+        + Length<typename Element<LeftT, RightT>::Tail >::result
     };
 };
 //==============================================================================
 template<class StackT, class RequiredT>
-struct IndexByType
+struct IndexOf
 {
     enum{
         result = -1
@@ -176,43 +176,47 @@ struct IndexByType
 };
 //------------------------------------------------------------------------------
 template<class T1, class T2, class T3>
-struct IndexByType<T1, Element<T2,T3> >
+struct IndexOf<T1, Element<T2,T3> >
 {
     enum{
-        result = (TypeEqual<typename Element<T2, T3>::HeadType, T1>::result) ? (0) : 
-        ((IndexByType<T1, typename Element<T2, T3>::TailType>::result == -1) ? (-1) : (IndexByType<T1, typename Element<T2, T3>::TailType>::result + 1))
+        result = 
+            (TypeEqual<typename Element<T2, T3>::Head, T1>::result) ? (0) : (
+                (IndexOf<T1, typename Element<T2, T3>::Tail>::result == -1)? 
+                (-1) : 
+                (IndexOf<T1, typename Element<T2, T3>::Tail>::result + 1)
+                )
     };
 };
 //------------------------------------------------------------------------------
 template<class T1, class T2, class T3>
-struct IndexByType<Element<T2, T3>, T1 >
+struct IndexOf<Element<T2, T3>, T1 >
 {
     enum{
-        result = IndexByType<T1, Element<T2, T3>>::result
+        result = IndexOf<T1, Element<T2, T3>>::result
     };
 };
 //==============================================================================
 template<class StackT, num i>
-struct TypeByIndex
+struct TypeAt
 {
     typedef NullType ResultType;
 };
 //------------------------------------------------------------------------------
 template<class T1, class T2, num i>
-struct TypeByIndex<Element<T1,T2>, i>
+struct TypeAt<Element<T1,T2>, i>
 {
     typedef typename SelectType<
     i < 0,
     NullType,
-    typename TypeByIndex<typename Element<T1, T2>::TailType, i - 1 >::ResultType
+    typename TypeAt<typename Element<T1, T2>::Tail, i - 1 >::ResultType
     >
     ::ResultType ResultType;
 };
 //------------------------------------------------------------------------------
 template<class T1, class T2>
-struct TypeByIndex<Element<T1, T2>, 0>
+struct TypeAt<Element<T1, T2>, 0>
 {
-    typedef typename Element<T1, T2>::HeadType ResultType;
+    typedef typename Element<T1, T2>::Head ResultType;
 };
 //==============================================================================
 template<class StackT, class RemoveT>
@@ -225,11 +229,16 @@ template<class T1, class T2, class RemoveT>
 struct Remove<Element<T1, T2>, RemoveT>
 {
     typedef typename SelectType<
-    TypeEqual<typename Element<T1, T2>::HeadType, RemoveT>::result,
-    typename Element<T1, T2>::TailType,
-    Element<typename Element<T1, T2>::HeadType, typename Remove<typename Element<T1, T2>::TailType, RemoveT>::ResultStack >
-    >
-    ::ResultType ResultStack;
+        TypeEqual<typename Element<T1, T2>::Head, RemoveT>::result
+        ,
+        typename Element<T1, T2>::Tail
+        ,
+        Element<
+            typename Element<T1, T2>::Head,
+            typename Remove<typename Element<T1,T2>::Tail, RemoveT>::ResultStack
+            >
+        >
+        ::ResultType ResultStack;
 };
 //==============================================================================
 template<class StackT>
@@ -242,15 +251,132 @@ template<class T1, class T2>
 struct RemoveDuplicates<Element<T1,T2> >
 {
     typedef typename SelectType<
-    IndexByType<typename Element<T1, T2>::TailType, typename Element<T1, T2>::HeadType>::result >= 0,
-    typename RemoveDuplicates<typename Element<T1, T2>::TailType>::ResultStack,
-    Element<typename Element<T1, T2>::HeadType, typename RemoveDuplicates<typename Element<T1, T2>::TailType>::ResultStack>
-    >
-    ::ResultType ResultStack;
+        IndexOf<
+            typename Element<T1, T2>::Tail, 
+            typename Element<T1, T2>::Head
+            >::result != -1
+        ,
+        typename RemoveDuplicates<typename Element<T1, T2>::Tail>::ResultStack
+        ,
+        Element<
+            typename Element<T1, T2>::Head,
+            typename RemoveDuplicates<
+                typename Element<T1, T2>::Tail
+                >::ResultStack
+            >
+        >
+        ::ResultType ResultStack;
+};
+//==============================================================================
+template<class StackT, class T>
+struct Append
+{
+    typedef Element<StackT, T> ResultStack;
+};
+//==============================================================================
+template<class StackT, template <class,class> class CompareT> 
+struct FindMinimum
+{
+    typedef StackT ResultType;
+};
+//------------------------------------------------------------------------------
+template<class T1, class T2, template <class, class> class CompareT>
+struct FindMinimum<Element<T1,T2>, CompareT>
+{
+    typedef typename SelectType<
+        IsNullType<typename Element<T1, T2>::Tail>::result
+        ,
+        typename Element<T1, T2>::Head
+        ,
+        typename MinType<            
+            typename FindMinimum<typename Element<T1, T2>::Tail,CompareT>::ResultType,
+            typename Element<T1, T2>::Head,
+            CompareT
+            >::ResultType 
+        >::ResultType ResultType;
+};
+//==============================================================================
+template<class StackT, template <class, class> class CompareT>
+struct FindMaximum
+{
+    typedef StackT ResultType;
+};
+//------------------------------------------------------------------------------
+template<class T1, class T2, template <class, class> class CompareT>
+struct FindMaximum<Element<T1, T2>, CompareT>
+{
+    typedef typename SelectType<
+        IsNullType<typename Element<T1, T2>::Tail>::result
+        ,
+        typename Element<T1, T2>::Head
+        ,
+        typename MaxType<            
+            typename FindMaximum<typename Element<T1, T2>::Tail, CompareT>::ResultType,
+            typename Element<T1, T2>::Head,
+            CompareT
+            >::ResultType
+        >::ResultType ResultType;
+};
+//==============================================================================
+template<class StackT, template <class,class> class CompareT >
+struct SortDescending
+{
+    typedef StackT ResultStack;
+};
+//------------------------------------------------------------------------------
+template<class T1, class T2, template <class, class> class CompareT >
+struct SortDescending<Element<T1,T2>, CompareT>
+{
+    typedef typename SelectType <
+        IsNullType<typename Element<T1, T2>::Tail >::result
+        ,
+        Element<T1, T2>
+        ,
+        Element<
+            typename FindMaximum<Element<T1, T2>, CompareT>::ResultType
+            ,
+            typename SortDescending< 
+                typename Remove<
+                    Element<T1, T2>, 
+                    typename FindMaximum<Element<T1, T2>, CompareT>::ResultType
+                    >::ResultStack,
+                CompareT
+                >::ResultStack
+            >
+        >::ResultType ResultStack;
+};
+//==============================================================================
+template<class StackT, template <class, class> class CompareT >
+struct SortAscending
+{
+    typedef StackT ResultStack;
+};
+//------------------------------------------------------------------------------
+template<class T1, class T2, template <class, class> class CompareT >
+struct SortAscending<Element<T1, T2>, CompareT>
+{
+    typedef typename SelectType <
+        IsNullType<typename Element<T1, T2>::Tail >::result
+        ,
+        Element<T1, T2>
+        ,
+        Element<
+            typename FindMinimum<Element<T1, T2>, CompareT>::ResultType
+            ,
+            typename SortAscending<
+                typename Remove<
+                    Element<T1, T2>,
+                    typename FindMinimum<Element<T1, T2>, CompareT>::ResultType
+                    >::ResultStack,
+                CompareT
+                >::ResultStack
+            >
+        > ::ResultType ResultStack;
 };
 //==============================================================================
 } // TypeStack
 } // Bikes
 
 #endif // <- INCLUDE_BIKES_TYPECOLLECTING_TYPESTACK_H
+
 
